3d351ea3dd80b18d846d2f14f37c0c73511aad0b
mekniiemna <97346096+mekniemna@users.noreply.github.com>
2024-03-29 20:30:47 +0100

    first commit

---
+++ src/test/java/com/esprit/devops_project/DevopsProjectApplicationTests.java
@@ -0,0 +1,13 @@
+package com.esprit.devops_project;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DevopsProjectApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
